baf3963abf6d4efa5f4a001305241254449c3018
Anonymousfrom94 <ousall535@gmail.com>
2025-01-29 20:30:37 +0100

    tp-string fini

--- TP_1/tp-string.js
+++ TP_1/tp-string.js
@@ -4,35 +4,54 @@ module.exports = {
   // 1) Séparer l'email en deux parties au niveau du caractère '@', renvoyer le tableau correspondant
   Q1() {
     // Implémentation ici
+    monEmail = email;
+    tab = monEmail.split('@');
+    return tab;
   },
 
   // 2) Vérifier qu'il n'y a qu'un seul caractère '@' dans email. Renvoyer un booléen
   Q2() {
     // Implémentation ici
+    monEmail = email;
+    return monEmail.includes("@");
   },
 
   // 3) Renvoyer l'index du caractère '@'
   Q3() {
     // Implémentation ici
+    monEmail = email;
+    index = monEmail.indexOf("@");
+    return index;
   },
 
   // 4) Renvoyer la sous-chaîne se situant après le caractère '@'
   Q4() {
     // Implémentation ici
+    monEmail = email;
+    tab = monEmail.split('@');
+    return tab.at(1);
   },
 
   // 5) Renvoyer la variable en majuscule
   Q5() {
     // Implémentation ici
+    monEmail = email;
+    monEmail.toUpperCase();
+    return monEmail;
   },
 
   // 6) Renvoyer le type de la variable email
   Q6() {
     // Implémentation ici
+    monEmail = email;
+    return typeof(monEmail);
   },
 
   // 7) Changez email.com par gmail.com, renvoyez la chaine modifiée
   Q7() {
     // Implémentation ici
+    monEmail = email;
+    monEmail.replace("email", "gmail");
+    return monEmail;
   }
 };
