0ac22b1660a1a06a3196eb9e9c480dbbb624b786
oumar sall <ousall535@gmail.com>
2025-03-06 10:40:00 +0100

    Ajout formulaire sans back et style

---
+++ TP/frontend/src/app/pages/create-book-page/create-book-form/create-book-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-book-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-book-form.component.html',
+  styleUrl: './create-book-form.component.css'
+})
+export class CreateBookFormComponent {
+
+}
--- TP/frontend/src/app/pages/create-book-page/create-book-page.component.ts
+++ TP/frontend/src/app/pages/create-book-page/create-book-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CreateBookFormComponent } from './create-book-form/create-book-form.component';
 
 @Component({
   selector: 'app-create-book-page',
   standalone: true,
-  imports: [],
+  imports: [CreateBookFormComponent],
   templateUrl: './create-book-page.component.html',
   styleUrl: './create-book-page.component.css',
 })
